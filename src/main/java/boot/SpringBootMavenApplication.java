426f009852f0825830417763dd7b3aaba96300d9
kkh <kkh@DESKTOP-IO533O7>
2020-11-02 00:51:35 +0900

    커밋

---
+++ src/main/java/boot/SpringBootMavenApplication.java
@@ -0,0 +1,13 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenApplication.class, args);
+	}
+
+}
